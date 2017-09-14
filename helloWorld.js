093d175dce80a74625f3c5aebe3c1218a864921b
Hank <aken1215@hotmail.com>
2017-09-14 09:36:14 +0800

    測試將bot相依性放入helloWorld

--- helloWorld.js
+++ helloWorld.js
@@ -1,4 +1,16 @@
 var http = require('http');
+var linebot = require('linebot');
+
+var bot = linebot({
+    //channelId: "1530245573" ,
+    //channelSecret: "cebc3f2a9f76e985607de7f855794fbe",
+    //channelAccessToken: "RQ22Ia0B9nF86lFPdDrVEQiawPOxhC0iBakGupdgPQRSDs9KKlKN56THLEGxJK5J9hozRTJ4bSTusVspBDj3NSSx9jLN5cZWI/8EJbCJjUpk28v3E2VZBlEsgx+L2U8JdmmeIhFhhBqwCoNOWoAUNwdB04t89/1O/w1cDnyilFU="
+     channelId: "1535163663" ,
+     channelSecret: "60e70aa9ae539ac86e4c925b73428afd",
+     channelAccessToken: "bKUNGIxsMvf6tZbGi0W/+ThpuPxXnLqsawb7Y7P1dhzArvFCWS+jnyVCmkuBKrO0uRdhMI+A+1Y8c3ojzc+1/8JWof0grGGxbCQ5JUb1s2RobAAsDOtoQ5AcpoqTdQyM6TrM58kPwhLukdWAEi/zewdB04t89/1O/w1cDnyilFU="
+   });
+   
+
 
 var server = http.createServer(function(request, response) {
 
